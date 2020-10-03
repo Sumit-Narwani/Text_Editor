4e187d744ac4214f01e538c372184f2d7d2a81dc
Sumit Narwani <narwani95@gmail.com>
2020-10-03 10:29:34 +0530

    added html file and script

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 import { Stack } from './stack.js';
 
+// This event prevents the user from using the ctrl operations(ctrl+c, ctrl+v)
 document.onkeydown = function(event) {
     if (event.ctrlKey || event.metaKey) {
         event.preventDefault();
@@ -14,14 +15,23 @@ onload = function () {
     const clear = document.getElementById('clear');
     const temptext = document.getElementById('temptext');
 
+    // textbox is the writing here for the editor
     textbox.value = "";
+
+    // text is the varible we have created to store the string (of textbox)
     let text = "";
+
+    // stack object created
     let stack = new Stack();
 
+    // To avoid user from writing in between the string in the textbox
+    // User can only write at the start or at the end of the string.
     textbox.onclick = function () {
         textbox.selectionStart = textbox.selectionEnd = textbox.value.length;
     };
 
+
+    // Function assosciated with the clear button (event -> onclick)
     clear.onclick = function () {
         stack.clear();
         text = "";
@@ -29,8 +39,10 @@ onload = function () {
         temptext.innerHTML = "Sequence of operations will be shown here !";
     };
 
+
+    // Function that detects the event whether we are inserting or deleting.
     textbox.oninput = function(event){
-        // console.log(event);
+        //console.log(event);
         switch(event.inputType){
             case "insertText":
                 stack.push(0, event.data);
@@ -40,18 +52,31 @@ onload = function () {
                 break;
         }
 
+        // Operations are written the temptext HTML
         temptext.innerHTML = "On stack "+stack.top()+"<br>"+temptext.innerHTML;
         text = textbox.value;
     };
 
+
+    // Function for the undo operations ( Insertion and Deletion )
     undo.onclick = function () {
+        // top element is stored in the operation variable
         let operation = stack.pop();
+
+        // Checking whether the stack is empty
         if(operation[0]!==-1){
             temptext.innerHTML = "Performing undo operation<br>"+temptext.innerHTML;
+            
+            // Check for the insertion operation (type-0)
             if(operation[0] === 0){
                 let len = operation[1].length;
+
+                // removing the characters of as much length as inserted in the previous insert operation
                 textbox.value = textbox.value.substring(0,textbox.value.length-len);
-            } else{
+            } 
+            
+            // Case for deletion thus here we just add/append the deleted element to the textbox value1
+            else{
                 textbox.value += operation[1];
             }
             text = textbox.value;
--- stack.js
+++ stack.js
@@ -1,6 +1,3 @@
-/**
- * Created by aarnavjindal on 30/03/20.
- */
 
 export { Stack }
 
@@ -11,39 +8,60 @@ class Stack{
         this.stack = [];
     }
 
+    // This function will clear the stack
     clear(){
         this.size = 0;
         this.stack = [];
     }
 
+
+    // This function will check whether the stack is empty or not
     isEmpty(){
         return  ( this.size === 0 );
     }
 
+    // This function will return the top element of the stack
     top(){
         return this.stack[this.size-1];
     }
 
+
+    // pop function will remove(delete) the top element if the stack is not empty.
     pop(){
         if(!this.isEmpty()) {
             this.size--;
             return this.stack.pop();
         } else{
+            // custom defined null value for pop operation if the stack is already empty.
             return [-1,''];
         }
     }
 
+    // push operation for inserting new elements in the stack.
     push(type, char){
+
+        // If the stack is empty and the opeation is insert (type=0).
         if(this.isEmpty()){
             if(type===0)
                 this.stack.push([type, char]);
         } else{
+            // tmp will now have the top element.
             let tmp = this.top();
+            
+            // Check if the top element operation and current operation is same or not.
+            // Also we check is the buffer length of the top element is not full then 
+            // only we can append a new element in that.
+             
             if(tmp[0]===type && tmp[1].length < this.buffer){
                 let top = this.pop();
                 top[1] = char + top[1];
                 this.stack.push(top);
-            } else{
+            } 
+            
+            // Last case when the operation is different(type) or the buffer of previous 
+            // top is full then we add new element at the top of the stack.
+            
+            else{
                 this.stack.push([type, char]);
             }
         }
